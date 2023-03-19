df186000aedbc3fdf50d620ced910c0d1c4e5fe6
Tapan Das <tapands.das@gmail.com>
2023-03-19 23:22:35 +0530

    Change Skill to functional component

--- src/components/intake/Skill.js
+++ src/components/intake/Skill.js
@@ -1,74 +1,35 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import GeneralForm from "./GeneralForm";
 import Highlight from "./Highlight";
 
-class Skill extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      skills: [],
-    };
+function Skill({ updateSkills }) {
+  const savedJson = localStorage.getItem("skills");
+  const storedSkills = JSON.parse(savedJson);
 
-    this.changeSkills = this.changeSkills.bind(this);
-    this.deleteSkills = this.deleteSkills.bind(this);
-  }
+  const [skills, setSkills] = useState(storedSkills || []);
 
-  componentDidMount() {
-    if (localStorage.getItem("skills")) {
-      const savedJson = localStorage.getItem("skills");
-      const skills = JSON.parse(savedJson);
+  useEffect(() => {
+    updateSkills(skills);
+    localStorage.setItem("skills", JSON.stringify(skills));
+  }, [skills, updateSkills]);
 
-      this.setState(
-        {
-          ...skills,
-        },
-        () => {
-          this.props.updateSkills(this.state.skills);
-        }
-      );
-    }
-  }
+  const changeSkills = (newSkill) => {
+    setSkills(skills.concat(newSkill));
+    updateSkills(skills);
+  };
 
-  componentDidUpdate() {
-    if (!localStorage.getItem("skills")) {
-      localStorage.setItem("skills", JSON.stringify(this.state));
-    } else {
-      localStorage.removeItem("skills");
-      localStorage.setItem("skills", JSON.stringify(this.state));
-    }
-  }
+  const deleteSkills = (id) => {
+    setSkills(skills.filter((skill) => skill.id !== id));
+    updateSkills(skills);
+  };
 
-  changeSkills(newSkill) {
-    this.setState(
-      {
-        skills: this.state.skills.concat(newSkill),
-      },
-      () => {
-        this.props.updateSkills(this.state.skills);
-      }
-    );
-  }
-
-  deleteSkills(id) {
-    this.setState(
-      (prevState) => ({
-        skills: prevState.skills.filter((skill) => skill.id !== id),
-      }),
-      () => {
-        this.props.updateSkills(this.state.skills);
-      }
-    );
-  }
-
-  render() {
-    return (
-      <div className="skills">
-        <h3>Skills</h3>
-        <Highlight items={this.state.skills} deleteItem={this.deleteSkills} />
-        <GeneralForm subject={"Skill"} updateSection={this.changeSkills} />
-      </div>
-    );
-  }
+  return (
+    <div className="skills">
+      <h3>Skills</h3>
+      <Highlight items={skills} deleteItem={deleteSkills} />
+      <GeneralForm subject={"Skill"} updateSection={changeSkills} />
+    </div>
+  );
 }
 
 export default Skill;
