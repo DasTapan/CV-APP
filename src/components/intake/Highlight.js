d6dc7dc6600ae19dba05dfe74c421e794a6e07bc
Tapan Das <tapands.das@gmail.com>
2023-03-19 20:07:45 +0530

    Change Highlight into functional component

--- src/components/intake/Highlight.js
+++ src/components/intake/Highlight.js
@@ -1,33 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Highlight extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    const [...items] = this.props.items;
-    const { deleteItem } = this.props;
-
-    return (
-      <div className="highlight">
-        {items.map((item) => {
-          return (
-            <div className="highlight-item" key={item.id}>
-              <span>{item.text}</span>
-              <button
-                type="button"
-                onClick={() => {
-                  deleteItem(item.id);
-                }}
-              ></button>
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
+function Highlight({ items, deleteItem }) {
+  return (
+    <div className="highlight">
+      {items.map((item) => {
+        return (
+          <div className="highlight-item" key={item.id}>
+            <span>{item.text}</span>
+            <button
+              type="button"
+              onClick={() => {
+                deleteItem(item.id);
+              }}
+            ></button>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Highlight;
