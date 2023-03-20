3bb4663c1a1845c0246002737a20aecf222d810f
Tapan Das <tapands.das@gmail.com>
2023-03-20 12:01:41 +0530

    Change Education into functional component

--- src/components/intake/Education.js
+++ src/components/intake/Education.js
@@ -1,139 +1,87 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Highlight from "./Highlight";
 import uniqid from "uniqid";
 
-class Education extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: {
-        text: "",
-        loc: "",
-        university: "",
-        id: uniqid(),
-      },
-      education: [],
-    };
+function Education({ updateEducation }) {
+  const savedJson = localStorage.getItem("eduInfo");
+  const storedEducation = JSON.parse(savedJson);
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.deleteEducation = this.deleteEducation.bind(this);
-  }
+  const [education, setEducation] = useState(storedEducation || []);
+  const [text, setText] = useState(""); //naming it 'text' only to assign in Highlight (other component store in 'text')
+  const [loc, setLoc] = useState("");
+  const [university, setUniversity] = useState("");
+  const [id, setId] = useState(uniqid());
 
-  componentDidMount() {
-    if (localStorage.getItem("eduInfo")) {
-      const savedJson = localStorage.getItem("eduInfo");
-      const eduInfo = JSON.parse(savedJson);
+  useEffect(() => {
+    updateEducation(education);
 
-      this.setState(
-        {
-          ...eduInfo,
-        },
-        () => {
-          this.props.updateEducation(this.state.education);
-        }
-      );
-    }
-  }
+    localStorage.setItem("eduInfo", JSON.stringify(education));
+  }, [education, updateEducation]);
 
-  componentDidUpdate() {
-    if (!localStorage.getItem("eduInfo")) {
-      localStorage.setItem("eduInfo", JSON.stringify(this.state));
-    } else {
-      localStorage.removeItem("eduInfo");
-      localStorage.setItem("eduInfo", JSON.stringify(this.state));
-    }
-  }
-
-  handleChange(event) {
-    const target = event.target;
-    const name = target.name;
-    const value = target.value;
-
-    this.setState((prevState) => ({
-      value: {
-        ...prevState.value,
-        [name]: value,
-      },
-    }));
-  }
-
-  handleSubmit(event) {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    this.setState(
-      {
-        value: {
-          text: "",
-          loc: "",
-          university: "",
-          id: uniqid(),
-        },
-        education: this.state.education.concat(this.state.value),
-      },
-      () => {
-        this.props.updateEducation(this.state.education);
-      }
-    );
-  }
+    setEducation(education.concat({ text, loc, university, id }));
+    updateEducation(education);
 
-  deleteEducation(id) {
-    this.setState(
-      (prevState) => ({
-        education: prevState.education.filter((edu) => edu.id !== id),
-      }),
-      () => {
-        this.props.updateEducation(this.state.education);
-      }
-    );
-  }
+    setText("");
+    setLoc("");
+    setUniversity("");
+    setId(uniqid);
+  };
 
-  render() {
-    return (
-      <div className="education">
-        <h3>Education</h3>
-        <Highlight
-          items={this.state.education}
-          deleteItem={this.deleteEducation}
-        />
-        <form action="" onSubmit={this.handleSubmit}>
-          <h5>Add Education:</h5>
-          <div className="form-row">
-            <label htmlFor="">Degree :</label>
-            <input
-              type="text"
-              name="text"
-              onChange={this.handleChange}
-              value={this.state.value.text}
-              required
-            />
-          </div>
-          <div className="form-row">
-            <label htmlFor="">University :</label>
-            <input
-              type="text"
-              name="university"
-              onChange={this.handleChange}
-              value={this.state.value.university}
-              required
-            />
-          </div>
-          <div className="form-row">
-            <label htmlFor="">State :</label>
-            <input
-              type="text"
-              name="loc"
-              onChange={this.handleChange}
-              value={this.state.value.loc}
-              required
-            />
-          </div>
-          <div className="form-row submit-btn">
-            <button type="submit">Add</button>
-          </div>
-        </form>
-      </div>
-    );
-  }
+  const deleteEducation = (id) => {
+    setEducation(education.filter((degree) => degree.id !== id));
+    updateEducation(education);
+  };
+
+  return (
+    <div className="education">
+      <h3>Education</h3>
+      <Highlight items={education} deleteItem={deleteEducation} />
+      <form action="" onSubmit={handleSubmit}>
+        <h5>Add Education:</h5>
+        <div className="form-row">
+          <label htmlFor="">Degree :</label>
+          <input
+            type="text"
+            name="text"
+            onChange={(e) => {
+              setText(e.target.value);
+            }}
+            value={text}
+            required
+          />
+        </div>
+        <div className="form-row">
+          <label htmlFor="">University :</label>
+          <input
+            type="text"
+            name="university"
+            onChange={(e) => {
+              setUniversity(e.target.value);
+            }}
+            value={university}
+            required
+          />
+        </div>
+        <div className="form-row">
+          <label htmlFor="">State :</label>
+          <input
+            type="text"
+            name="loc"
+            onChange={(e) => {
+              setLoc(e.target.value);
+            }}
+            value={loc}
+            required
+          />
+        </div>
+        <div className="form-row submit-btn">
+          <button type="submit">Add</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Education;
