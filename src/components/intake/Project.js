2cf5bd7d7f4b4477fcf3e3d03bb7e4ee6c7a1f5e
Tapan Das <tapands.das@gmail.com>
2023-03-20 11:02:01 +0530

    Change Project into functional component

--- src/components/intake/Project.js
+++ src/components/intake/Project.js
@@ -1,138 +1,86 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Highlight from "./Highlight";
 import uniqid from "uniqid";
 
-class Project extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: {
-        text: "", //naming it 'text' only to assign in Highlight (other component store in 'text')
-        subTitle: "",
-        description: "",
-        id: uniqid(),
-      },
-      projects: [],
-    };
+function Project({ updateProjects }) {
+  const savedJson = localStorage.getItem("projectInfo");
+  const storedProjects = JSON.parse(savedJson);
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.deleteProject = this.deleteProject.bind(this);
-  }
+  const [projects, setProjects] = useState(storedProjects.projects || []);
+  const [text, setText] = useState(""); //naming it 'text' only to assign in Highlight (other component store in 'text')
+  const [subTitle, setSubTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [id, setId] = useState(uniqid());
 
-  componentDidMount() {
-    if (localStorage.getItem("projectInfo")) {
-      const savedJson = localStorage.getItem("projectInfo");
-      const projectInfo = JSON.parse(savedJson);
+  useEffect(() => {
+    updateProjects(projects);
 
-      this.setState(
-        {
-          ...projectInfo,
-        },
-        () => {
-          this.props.updateProjects(this.state.projects);
-        }
-      );
-    }
-  }
+    localStorage.setItem("projectInfo", JSON.stringify(projects));
+  }, [projects, updateProjects]);
 
-  componentDidUpdate() {
-    if (!localStorage.getItem("projectInfo")) {
-      localStorage.setItem("projectInfo", JSON.stringify(this.state));
-    } else {
-      localStorage.removeItem("projectInfo");
-      localStorage.setItem("projectInfo", JSON.stringify(this.state));
-    }
-  }
-
-  handleChange(event) {
-    const target = event.target;
-    const name = target.name;
-    const value = target.value;
-
-    this.setState((prevState) => ({
-      value: {
-        ...prevState.value,
-        [name]: value,
-      },
-    }));
-  }
-
-  handleSubmit(event) {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    this.setState(
-      {
-        value: {
-          text: "",
-          subTitle: "",
-          description: "",
-          id: uniqid(),
-        },
-        projects: this.state.projects.concat(this.state.value),
-      },
-      () => {
-        this.props.updateProjects(this.state.projects);
-      }
-    );
-  }
+    setProjects(projects.concat({ text, subTitle, description, id }));
+    updateProjects(projects);
 
-  deleteProject(id) {
-    this.setState(
-      (prevState) => ({
-        projects: prevState.projects.filter((project) => project.id !== id),
-      }),
-      () => {
-        this.props.updateProjects(this.state.projects);
-      }
-    );
-  }
+    setText("");
+    setSubTitle("");
+    setDescription("");
+    setId(uniqid);
+  };
 
-  render() {
-    return (
-      <div className="projects">
-        <h3>Projects</h3>
-        <Highlight
-          items={this.state.projects}
-          deleteItem={this.deleteProject}
-        />
-        <form action="" onSubmit={this.handleSubmit}>
-          <h5>Add Project:</h5>
-          <div className="form-row">
-            <label htmlFor="">Title :</label>
-            <input
-              type="text"
-              name="text"
-              value={this.state.value.text}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
-          <div className="form-row">
-            <label htmlFor="">Sub-Title :</label>
-            <input
-              type="text"
-              name="subTitle"
-              value={this.state.value.subTitle}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
-          <div className="form-row">
-            <label htmlFor="">Description :</label>
-            <textarea
-              name="description"
-              value={this.state.value.description}
-              onChange={this.handleChange}
-              required
-            ></textarea>
-          </div>
-          <div className="form-row submit-btn">
-            <button type="submit">Add</button>
-          </div>
-        </form>
-      </div>
-    );
-  }
+  const deleteProject = (id) => {
+    setProjects(projects.filter((project) => project.id !== id));
+    updateProjects(projects);
+  };
+
+  return (
+    <div className="projects">
+      <h3>Projects</h3>
+      <Highlight items={projects} deleteItem={deleteProject} />
+      <form action="" onSubmit={handleSubmit}>
+        <h5>Add Project:</h5>
+        <div className="form-row">
+          <label htmlFor="">Title :</label>
+          <input
+            type="text"
+            name="text"
+            value={text}
+            onChange={(e) => {
+              setText(e.target.value);
+            }}
+            required
+          />
+        </div>
+        <div className="form-row">
+          <label htmlFor="">Sub-Title :</label>
+          <input
+            type="text"
+            name="subTitle"
+            value={subTitle}
+            onChange={(e) => {
+              setSubTitle(e.target.value);
+            }}
+            required
+          />
+        </div>
+        <div className="form-row">
+          <label htmlFor="">Description :</label>
+          <textarea
+            name="description"
+            value={description}
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
+            required
+          ></textarea>
+        </div>
+        <div className="form-row submit-btn">
+          <button type="submit">Add</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Project;
